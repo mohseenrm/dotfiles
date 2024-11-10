7f3f6773d99260f267f4430651d4ec530af1cf64
mohseenrm <mohseenmukaddam6@gmail.com>
2024-11-09 17:08:13 -0800

    update lua line style

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -107,44 +107,61 @@ end
 
 local lualine = require("lualine")
 
-lualine.setup({
+local colors = {
+  blue = "#86e1fc",
+  cyan = "#4fd6be",
+  black = "#080808",
+  white = "#c6c6c6",
+  red = "#ff5189",
+  violet = "#d183e8",
+  grey = "#303030",
+}
+
+local bubbles_theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.cyan },
+    b = { fg = colors.white, bg = colors.grey },
+    c = { fg = colors.white },
+  },
+
+  insert = { a = { fg = colors.black, bg = colors.blue } },
+  visual = { a = { fg = colors.black, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.white, bg = colors.black },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white },
+  },
+}
+
+require("lualine").setup({
   options = {
-    icons_enabled = true,
-    theme = "auto",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    },
+    theme = bubbles_theme,
+    component_separators = "",
+    section_separators = { left = "", right = "" },
   },
   sections = {
-    lualine_a = { "mode" },
+    lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
     lualine_b = { "branch", "diff", "diagnostics", { "macro-recording", fmt = show_macro_recording } },
-    lualine_c = { { "filename", file_status = true, path = 1 } },
-    lualine_x = { "encoding", "fileformat", "filetype" },
-    lualine_y = { "progress" },
-    lualine_z = { "location" },
+    lualine_c = {
+      "%=", --[[ add your center component here in place of this comment ]]
+    },
+    lualine_x = {},
+    lualine_y = { { "filename", file_status = true, path = 1 }, "progress" },
+    lualine_z = {
+      { "location", separator = { right = "" }, left_padding = 2 },
+    },
   },
   inactive_sections = {
-    lualine_a = {},
+    lualine_a = { "filename" },
     lualine_b = {},
-    lualine_c = { "filename" },
-    lualine_x = { "location" },
+    lualine_c = {},
+    lualine_x = {},
     lualine_y = {},
-    lualine_z = {},
+    lualine_z = { "location" },
   },
   tabline = {},
-  winbar = {},
-  inactive_winbar = {},
   extensions = {},
 })
 
