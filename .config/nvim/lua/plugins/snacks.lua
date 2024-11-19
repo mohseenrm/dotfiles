ee724a0bbd873c164a1e464b1fae1d4535236162
mohseenrm <mohseenmukaddam6@gmail.com>
2024-11-18 16:16:57 -0800

    add search marks feature

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -40,6 +40,23 @@ return {
           },
         },
       },
+      preset = {
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "m", desc = "Search Marks", action = "<leader>sm" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
     },
     quickfile = { enabled = true },
     statuscolumn = { enabled = true },
