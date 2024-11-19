6cdec9927147448eee15c742cde1d957019ead45
mohseenrm <mohseenmukaddam6@gmail.com>
2024-11-18 16:06:05 -0800

    add rosie image ❤️

--- .config/nvim/lua/plugins/dashboard-nvim.lua
+++ .config/nvim/lua/plugins/dashboard-nvim.lua
@@ -1,6 +1,7 @@
 return {
   "nvimdev/dashboard-nvim",
   event = "VimEnter",
+  enabled = false,
   opts = function()
     local lolcat = vim.fn.executable("lolcat") == 1
     local opts = {
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,3 +1,10 @@
+local home = os.getenv("HOME")
+local banner_path = home .. "/.config/nvim/logo/banner.txt"
+local banner_cmd = "cat " .. banner_path .. " | lolcat"
+
+local rosie_path = home .. "/.config/nvim/assets/rosie.png"
+local rosie_cmd = "chafa " .. rosie_path .. " --size 60X20 --format symbols --stretch"
+
 return {
   "folke/snacks.nvim",
   priority = 1000,
@@ -8,7 +15,32 @@ return {
     -- refer to the configuration section below
     bigfile = { enabled = true },
     notifier = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      width = 85,
+      pane_gap = 4,
+      sections = {
+        {
+          pane = 1,
+          {
+            section = "terminal",
+            cmd = banner_cmd,
+            indent = 8,
+            width = 75,
+            height = 10,
+          },
+          { section = "keys", gap = 1 },
+          { section = "startup" },
+        },
+        {
+          pane = 2,
+          {
+            section = "terminal",
+            cmd = rosie_cmd,
+            height = 30,
+          },
+        },
+      },
+    },
     quickfile = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
