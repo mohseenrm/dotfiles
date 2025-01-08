daecfec0a55dc21cae56ed34bf5be8c7a6f6e072
mohseenrm <mohseenmukaddam6@gmail.com>
2025-01-08 00:12:20 -0800

    update lock

--- .config/nvim/lua/plugins/barbecue.lua
+++ .config/nvim/lua/plugins/barbecue.lua
@@ -6,11 +6,10 @@ return {
     "SmiteshP/nvim-navic",
     "nvim-tree/nvim-web-devicons", -- optional dependency
   },
-  opts = {
-    -- configurations go here
-  },
+  opts = {},
   config = function()
     require("barbecue").setup({
+      theme = "tokyonight",
       -- create_autocmd = false, -- prevent barbecue from updating itself automatically
     })
 
