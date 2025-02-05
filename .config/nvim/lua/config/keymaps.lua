ebd688ad3d501bffa4201c9b6002827ededfa570
mohseenrm <mohseenmukaddam6@gmail.com>
2025-02-04 19:00:58 -0800

    nvim: purge fml

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -22,12 +22,5 @@ vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua require("persistence").loa
 -- restore the last session
 vim.api.nvim_set_keymap("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], {})
 
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>xfG",
-  [[<cmd>lua require('telescope.builtin').find_files({ cwd = require("telescope.utils").buffer_dir() })<cr>]],
-  {}
-)
-
 -- start oil
 vim.api.nvim_set_keymap("n", "<leader>o", [[<cmd>Oil<cr>]], { desc = "Start Oil" })
