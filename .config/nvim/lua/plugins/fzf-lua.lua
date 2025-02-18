c50217e4bbb94bac4e8a730bc806c558e9138160
mohseenrm <mohseenmukaddam6@gmail.com>
2025-02-17 21:14:40 -0800

    nvim: search and show hidden files by default

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -2,6 +2,12 @@ return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
+    files = {
+      hidden = true,
+    },
+    grep = {
+      hidden = true,
+    },
     keymap = {
       fzf = {
         ["ctrl-u"] = "half-page-up",
