410cf728039a92a5bec1ea7accc9764e39c7b1da
mohseenrm <mohseenmukaddam6@gmail.com>
2025-04-10 16:57:25 -0700

    nvim: try out virtual lines

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.diagnostic.config({ virtual_lines = true })
 vim.api.nvim_set_hl(0, "HighLight", {
   fg = "#f7768e",
 })
