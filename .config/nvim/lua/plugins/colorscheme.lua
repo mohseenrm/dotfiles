47b5bbfa63114533d507795acbb4f4ff6b917b97
mohseenrm <mohseenmukaddam6@gmail.com>
2025-07-01 00:09:55 -0700

    nvim: change theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,27 @@
 return {
   -- add tokyonight
   {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    opts = { style = "dragon" },
+    enabled = false,
+  },
+  {
     "folke/tokyonight.nvim",
     lazy = true,
     opts = { style = "night" },
-    enabled = true,
+    enabled = false,
   },
   {
     "nyoom-engineering/oxocarbon.nvim",
     lazy = true,
-    enabled = false,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = true,
-    enabled = false,
+    enabled = true,
   },
   -- Configure LazyVim to load prefered colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "oxocarbon",
     },
   },
 }
