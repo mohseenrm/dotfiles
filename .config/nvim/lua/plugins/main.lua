cd12b7377beb0e71a2d494e05b60ccc49dc36771
mohseenrm <mohseenmukaddam6@gmail.com>
2025-08-11 13:22:33 -0700

    nvim: refactor remove dead config

--- .config/nvim/lua/plugins/main.lua
+++ .config/nvim/lua/plugins/main.lua
@@ -32,25 +32,6 @@ return {
         desc = "Find Plugin File",
       },
     },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-        vimgrep_arguments = {
-          'rg',
-          '--color=never',
-          '--no-heading',
-          '--with-filename',
-          '--line-number',
-          '--column',
-          '--smart-case',
-          '--hidden',
-        },
-      },
-    },
 
     dependencies = {
       {
