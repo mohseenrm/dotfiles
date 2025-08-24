3213ea39f619a532aa0766bd6e8e5a648cbc121b
mohseenrm <mohseenmukaddam6@gmail.com>
2025-08-23 19:05:22 -0700

    nvim: update noice to not intercept lsp signatures

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -3,14 +3,24 @@ return {
   lazy = true,
   event = "BufReadPre",
   opts = {
-    -- add any options here
+    lsp = {
+      signature = {
+        enabled = false,
+      },
+      hover = {
+        enabled = false,
+      },
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = false,
+      lsp_doc_border = false,
+    },
   },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
   },
 }
