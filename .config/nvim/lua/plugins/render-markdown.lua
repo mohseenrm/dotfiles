64363baa996f4bd04bca39de76a9519f3a8d9492
mohseenrm <mohseenmukaddam6@gmail.com>
2025-08-23 22:34:01 -0700

    nvim: fix annoying issue with render-markdown hijacking styles

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -9,16 +9,38 @@ return {
   -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
   ---@module "render-markdown"
   ---@type render.md.UserConfig
-  event = "VeryLazy",
+  event = "BufReadPre *.md,*.mdx,*.markdown",
   lazy = true,
-  opts = {},
+  opts = {
+    completions = {
+      blink = {
+        enabled = true,
+      },
+    },
+  },
   config = function()
     require("obsidian").get_client().opts.ui.enable = false
     local obsidianNS = vim.api.nvim_get_namespaces()["ObsidianUI"]
     if not isempty(obsidianNS) then
       vim.api.nvim_buf_clear_namespace(0, obsidianNS, 0, -1)
     end
-    -- vim.api.nvim_buf_clear_namespace(0, vim.api.nvim_get_namespaces()["ObsidianUI"], 0, -1)
-    require("render-markdown").setup({})
+
+    require("render-markdown").setup {}
+    
+    -- Auto-enable/disable render-markdown based on filetype
+    vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
+      callback = function()
+        vim.schedule(function()
+          local ft = vim.bo.filetype
+          local success, _ = pcall(function()
+            if ft == "markdown" then
+              vim.cmd("RenderMarkdown enable")
+            elseif ft ~= "" then
+              vim.cmd("RenderMarkdown disable")
+            end
+          end)
+        end)
+      end,
+    })
   end,
 }
