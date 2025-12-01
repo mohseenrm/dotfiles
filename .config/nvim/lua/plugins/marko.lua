8a6b6b3537ebfe617832b964e6bfd1c130668fe0
mohseenrm <mohseenmukaddam6@gmail.com>
2025-11-30 23:56:40 -0800

    nvim: update lock

--- .config/nvim/lua/plugins/marko.lua
+++ .config/nvim/lua/plugins/marko.lua
@@ -2,6 +2,15 @@ return {
   "mohseenrm/marko.nvim",
   -- lazy = true,
   -- event = "VeryLazy",
+  --
+  -- "marko.nvim",
+  -- name = "marko.nvim",
+  -- dev = true,
+  -- dir = "~/Projects/marko.nvim",
+  enabled = true,
+  priority = 1000,
+  lazy = false,
+  opts = {},
   config = function()
     require("marko").setup()
   end,
