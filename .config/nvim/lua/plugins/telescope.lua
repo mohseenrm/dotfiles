84794c17de2ef228a0ecc9dd62a25141c3f56cef
mohseenrm <mohseenmukaddam6@gmail.com>
2026-03-09 15:51:57 -0700

    Fix(nvim): lint

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,12 @@
 return {
-   'nvim-telescope/telescope.nvim', version = '*',
-   enabled = true,
-   priority = 1000,
-   lazy = false,
-   dependencies = {
-     'nvim-lua/plenary.nvim',
-     -- optional but recommended
-     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-   }
+  "nvim-telescope/telescope.nvim",
+  version = "*",
+  enabled = true,
+  priority = 1000,
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    -- optional but recommended
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
 }
